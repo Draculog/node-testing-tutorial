b2c0e23cfc8c62d20cf8c396f11522e2e8be57fc
Vince <vincentmichaelpalma@gmail.com>
2025-12-03 21:35:09 -0800

    removed superbase useage in tests

--- solution-files/api-test-solution.js
+++ solution-files/api-test-solution.js
@@ -1,6 +1,5 @@
 const { expect } = require('chai');
-const request = require('supertest');
-const { startServer, stopServer, BASE_URL } = require('./utils/testServer');
+const { startServer, stopServer } = require('./utils/testServer');
 const { clearTasks } = require('./utils/testDB');
 
 describe('Tasks API', function () {
@@ -34,19 +33,34 @@ describe('Tasks API', function () {
     // TODO: Find the bug in this test and fix it
     // Hint: Run 'npm test'
 
+
     // Step 1) Create the Task object we want to add to the database
     const task = { description: 'Learn testing', completion_date: '2025-12-31' };
     
+
     // Step 2) Add the task object to the database and store the response object and its database ID in variables
-    const postResponse = await request(BASE_URL).post('/tasks').send(task);
-    const id = postResponse.body.id;
+    const postUrl = `http://127.0.0.1:3000/tasks`;
+    const postResponse = await fetch(postUrl, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(task)
+    });
+    const postData = await postResponse.json();
+    const id = postData.id;
+
 
     // Step 3) Call the route that gets the specific task from the database
     // and assert the Task object has the correct description and completion_date
-    const getResponse = await request(BASE_URL).get(`/tasks/${id}`);
-    expect(getResponse.body).to.include({ description: 'Learn testing', completion_date: '2025-12-31' }); // Solution here: completion_date is '2025-12-31' instead of '2025-12-30'
+    const getUrl = `http://127.0.0.1:3000/tasks/${id}`;
+    const getResponse = await fetch(getUrl)
+    const getData = await getResponse.json();
 
+    expect(getData).to.include({ description: 'Learn testing', completion_date: '2025-12-31' });
   });
+
+
+
+
 
   // #2) Complete this test by updating the description of an existing task in the database, 
   // updating it in the database, and verifying that the update in the database was successful.
@@ -55,26 +69,46 @@ describe('Tasks API', function () {
 
     // Step 1) The task object is being added to the database
     const task = { description: 'Learn testing', completion_date: '2025-12-31' };
-    const postResponse = await request(BASE_URL).post('/tasks').send(task);
-    const id = postResponse.body.id;
+
+    const postUrl = `http://127.0.0.1:3000/tasks`;
+    const postResponse = await fetch(postUrl, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(task)
+    });
+    const postData = await postResponse.json();
+    const id = postData.id;
+
 
     // TODO 
     // Step 2) Edit the task's description and call the UPDATE route to update it in the database
     // Hint: It is similar to how a Task is added to the database
-    // Pay attention to what kind of RESTful call you have to make
-    // (look at server/routes/api.js to what route updates task objects)
-    task.description = 'Updated testing';
-    await request(BASE_URL).put(`/tasks/${id}`).send(task);
+    // but pay attention to what kind of RESTful call you have to make
+    // (also look at server/routes/api.js to see which route updates task objects)
+    task.description = "Updated testing";
+
+    const putUrl = `http://127.0.0.1:3000/tasks/${id}`;
+    await fetch(putUrl, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(task)
+    });
+  
 
     // TODO 
-    // Step 3) Get the Task object from the database and store it in a variable
+    // Step 3) Get the Task object from the database and store it in a variable called getResponse
     // Hint: Look at previous tests to see how to call the route that gets a specific task from the database
-    const getResponse = await request(BASE_URL).get(`/tasks/${id}`);
+    const getUrl = `http://127.0.0.1:3000/tasks/${id}`;
+    const getResponse = await fetch(getUrl);
+    const getData = await getResponse.json();
+
 
     // Step 4) Assert the Task object called in Step 3 has the updated description
-    // Hint: Remember what kind of assertion should be used for validating variables
-    expect(getResponse.body.description).to.equal('Updated testing');
+    expect(getData.description).to.equal('Updated testing');
   });
+
+
+
 
 
   // #3) Write the entire test where you:
@@ -85,28 +119,46 @@ describe('Tasks API', function () {
     // TODO 
     // Step 1) Create a task object with description and completion_date and add it to the database
     // Hint: Call the route to add a task to the database like in previous tests
-    const task = { description: 'temp task', completion_date: '2025-12-31' };
-    const postResponse = await request(BASE_URL).post('/tasks').send(task);
-    const id = postResponse.body.id;
+    const task = { description: "temp task", completion_date: "2025-12-31" };
+
+    const postUrl = `http://127.0.0.1:3000/tasks`;
+    const postResponse = await fetch(postUrl, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(task)
+    });
+    const postData = await postResponse.json();
+    const id = postData.id;
+
 
     // TODO
     // Step 2) Assert the task exists in the database
     // Hint: Look back at previous tests to see how to assert an object exists in the database
-    const getResponse = await request(BASE_URL).get(`/tasks/${id}`);
-    expect(getResponse.body).to.include(task);
+    const getUrl = `http://127.0.0.1:3000/tasks/${id}`;
+    const getResponse = await fetch(getUrl)
+    const getData = await getResponse.json();
+    expect(getData).to.include(task);
+
 
     // TODO
     // Step 3) Delete the task you added to the database
     // Hint: Pay attention to what kind of RESTful call you have to make 
     // (look at server/routes/api.js to see which route deletes task objects)
-    await request(BASE_URL).delete(`/tasks/${id}`);
+    const deleteUrl = `http://127.0.0.1:3000/tasks/${id}`;
+    await fetch(deleteUrl, {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' },
+    });
+
 
     // TODO
     // Step 4) Assert that the task is deleted from the database
     // Hint: Do another GET request using the ID of the task you deleted,
     // keep the response in a variable, and assert its body to a value that indicates it does not exist
-    const empty_response = await request(BASE_URL).get(`/tasks/${id}`);
-    expect(empty_response.body).to.equal(null);
+    const emptyUrl = `http://127.0.0.1:3000/tasks/${id}`;
+    const emptyResponse = await fetch(emptyUrl)
+    const emptyData = await emptyResponse.json();
+    expect(emptyData).to.equal(null);
   });
 
 });
--- test/api.test.js
+++ test/api.test.js
@@ -1,6 +1,5 @@
 const { expect } = require('chai');
-const request = require('supertest');
-const { startServer, stopServer, BASE_URL } = require('./utils/testServer');
+const { startServer, stopServer } = require('./utils/testServer');
 const { clearTasks } = require('./utils/testDB');
 
 describe('Tasks API', function () {
@@ -34,19 +33,34 @@ describe('Tasks API', function () {
     // TODO: Find the bug in this test and fix it
     // Hint: Run 'npm test'
 
+
     // Step 1) Create the Task object we want to add to the database
     const task = { description: 'Learn testing', completion_date: '2025-12-31' };
     
+
     // Step 2) Add the task object to the database and store the response object and its database ID in variables
-    const postResponse = await request(BASE_URL).post('/tasks').send(task);
-    const id = postResponse.body.id;
+    const postUrl = `http://127.0.0.1:3000/tasks`;
+    const postResponse = await fetch(postUrl, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(task)
+    });
+    const postData = await postResponse.json();
+    const id = postData.id;
+
 
     // Step 3) Call the route that gets the specific task from the database
     // and assert the Task object has the correct description and completion_date
-    const getResponse = await request(BASE_URL).get(`/tasks/${id}`);
-    expect(getResponse.body).to.include({ description: 'Learn testing', completion_date: '2025-12-30' });
+    const getUrl = `http://127.0.0.1:3000/tasks/${id}`;
+    const getResponse = await fetch(getUrl)
+    const getData = await getResponse.json();
 
+    expect(getData).to.include({ description: 'Learn testing', completion_date: '2025-12-30' });
   });
+
+
+
+
 
   // #2) Complete this test by updating the description of an existing task in the database, 
   // updating it in the database, and verifying that the update in the database was successful.
@@ -55,8 +69,16 @@ describe('Tasks API', function () {
 
     // Step 1) The task object is being added to the database
     const task = { description: 'Learn testing', completion_date: '2025-12-31' };
-    const postResponse = await request(BASE_URL).post('/tasks').send(task);
-    const id = postResponse.body.id;
+
+    const postUrl = `http://127.0.0.1:3000/tasks`;
+    const postResponse = await fetch(postUrl, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(task)
+    });
+    const postData = await postResponse.json();
+    const id = postData.id;
+
 
     // TODO 
     // Step 2) Edit the task's description and call the UPDATE route to update it in the database
@@ -64,15 +86,20 @@ describe('Tasks API', function () {
     // but pay attention to what kind of RESTful call you have to make
     // (also look at server/routes/api.js to see which route updates task objects)
 
+  
 
     // TODO 
     // Step 3) Get the Task object from the database and store it in a variable called getResponse
     // Hint: Look at previous tests to see how to call the route that gets a specific task from the database
 
 
+
     // Step 4) Assert the Task object called in Step 3 has the updated description
-    expect(getResponse.body.description).to.equal('Updated testing');
+    expect(getData.description).to.equal('Updated testing');
   });
+
+
+
 
 
   // #3) Write the entire test where you:
@@ -83,11 +110,13 @@ describe('Tasks API', function () {
     // TODO 
     // Step 1) Create a task object with description and completion_date and add it to the database
     // Hint: Call the route to add a task to the database like in previous tests
- 
+   
+
 
     // TODO
     // Step 2) Assert the task exists in the database
     // Hint: Look back at previous tests to see how to assert an object exists in the database
+
 
 
     // TODO
@@ -96,11 +125,13 @@ describe('Tasks API', function () {
     // (look at server/routes/api.js to see which route deletes task objects)
 
 
+
     // TODO
     // Step 4) Assert that the task is deleted from the database
     // Hint: Do another GET request using the ID of the task you deleted,
     // keep the response in a variable, and assert its body to a value that indicates it does not exist
-  
+
+
     
   });
 
